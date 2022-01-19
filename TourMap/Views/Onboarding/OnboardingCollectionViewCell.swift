07ac68583d757de929ce702b016b99015cf581f5
Temitope <temitope.babatunde@decagon.dev>
2022-01-19 17:02:12 +0100

    added custom font

---
+++ TourMap/Views/Onboarding/OnboardingCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  OnboardingCollectionViewCell.swift
+//  TourMap
+//
+//  Created by Decagon on 19/01/2022.
+//
+
+import UIKit
+
+class OnboardingCollectionViewCell: UICollectionViewCell {
+    
+}
