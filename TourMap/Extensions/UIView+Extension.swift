601b0c3026552fcbdc94125e659db90051dd5b94
Temitope <temitope.babatunde@decagon.dev>
2022-01-19 10:41:03 +0100

    created onboarding view

--- TourMap/Extensions/UIView+Extension.swift
+++ TourMap/Extensions/UIView+Extension.swift
@@ -5,4 +5,13 @@
 //  Created by Decagon on 19/01/2022.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+  
+ @IBInspectable var cornerRadius : CGFloat {
+   get{ return self.cornerRadius}
+    set{self.layer.cornerRadius = newValue}
+  }
+}
+
--- TourMap/Views/Onboarding/OnboardingViewController.swift
+++ TourMap/Views/Onboarding/OnboardingViewController.swift
@@ -8,22 +8,13 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
-
-    override func viewDidLoad() {
+  
+  @IBOutlet weak var collectionView: UICollectionView!
+  
+  @IBOutlet weak var pageControl: UIPageControl!
+  
+  override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
