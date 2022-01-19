d09dc0210f046dedfdde17b650a3f0ad1c8e66b1
Temitope <temitope.babatunde@decagon.dev>
2022-01-19 17:04:22 +0100

    connect views to collectionViewCell

--- TourMap/Views/Onboarding/OnboardingCollectionViewCell.swift
+++ TourMap/Views/Onboarding/OnboardingCollectionViewCell.swift
@@ -8,5 +8,10 @@
 import UIKit
 
 class OnboardingCollectionViewCell: UICollectionViewCell {
-    
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var desciptionLabel: UILabel!
+  @IBOutlet weak var onboardingImageView: UIImageView!
+  
+  @IBAction func getStartedButton(_ sender: Any) {
+  }
 }
