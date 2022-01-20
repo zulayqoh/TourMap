fc783505b87337d0eecf3c5a81f5fcdceaa4f890
Temitope <temitope.babatunde@decagon.dev>
2022-01-20 10:49:21 +0100

    populated onboarding view cell

---
+++ TourMap/Onboarding/Models/OnboardingData.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingData.swift
+//  TourMap
+//
+//  Created by Decagon on 19/01/2022.
+//
+
+import Foundation
+import UIKit
+
+struct OnboardingData {
+  let title: String
+  let description: String
+  let image: String
+}
--- TourMap/Onboarding/Views/OnboardingCollectionViewCell.swift
+++ TourMap/Onboarding/Views/OnboardingCollectionViewCell.swift
@@ -8,9 +8,17 @@
 import UIKit
 
 class OnboardingCollectionViewCell: UICollectionViewCell {
+  
+  static var identifier = "OnboardingCollectionViewCell"
   @IBOutlet weak var titleLabel: UILabel!
   @IBOutlet weak var desciptionLabel: UILabel!
   @IBOutlet weak var onboardingImageView: UIImageView!
+  
+  func setupOnboardingView(with data: OnboardingData) {
+    titleLabel.text = data.title
+    desciptionLabel.text = data.description
+    onboardingImageView.image = UIImage(named: data.image)
+  }
   
   @IBAction func getStartedButton(_ sender: Any) {
   }
