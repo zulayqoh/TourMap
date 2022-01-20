61a100e3b0178694743276c76ec54bd20517466b
Temitope <temitope.babatunde@decagon.dev>
2022-01-20 14:01:44 +0100

    enabled pagecontroller visible scroll

--- TourMap/Onboarding/ViewModel/OnboardingViewModel.swift
+++ TourMap/Onboarding/ViewModel/OnboardingViewModel.swift
@@ -9,4 +9,5 @@ import Foundation
 
 class OnboardingViewModel {
   var data: [OnboardingData] = [OnboardingData(title: "Let's Travel", description: "\"Few places in this world are more dangerous than home. Fear not, therefore, to try the mountain passes. They will kill care, save you from deadly apathy, set you free, and call forth every faculty into vigorous, enthusiastic action.\" — John Muir", image: "travels1"), OnboardingData(title: "Plan A Trip", description: "\"It seems that the more places I see and experience, the bigger I realize the world to be. The more I become aware of, the more I realize how relatively little I know of it, how many places I have still to go, how much more there is to learn\" — Anthony Bourdain", image: "travels2"), OnboardingData(title: "Book A Flight", description: "\"Few places in this world are more dangerous than home. Fear not, therefore, to try the mountain passes. They will kill care, save you from deadly apathy, set you free, and call forth every faculty into vigorous, enthusiastic action.\" — John Muir", image: "travels3")]
+  var currentPage = 0
 }
--- TourMap/Onboarding/Views/OnboardingViewController.swift
+++ TourMap/Onboarding/Views/OnboardingViewController.swift
@@ -35,6 +35,12 @@ extension OnboardingViewController: UICollectionViewDataSource, UICollectionView
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
     CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
   }
+  func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+    let width = scrollView.frame.width
+    var activePage = viewModel.currentPage
+    activePage = Int(scrollView.contentOffset.x/width)
+    pageControl.currentPage = activePage
+  }
   
   
 }
