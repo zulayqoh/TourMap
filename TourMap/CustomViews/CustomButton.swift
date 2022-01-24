c7d4e81d0c30c1349570865418f4d3e13f30022a
Temitope <temitope.babatunde@decagon.dev>
2022-01-24 16:08:15 +0100

    create setup screen

--- TourMap/CustomViews/CustomButton.swift
+++ TourMap/CustomViews/CustomButton.swift
@@ -8,14 +8,23 @@
 import Foundation
 import UIKit
 
-class CustomButton: UILabel {
+class CustomButton: UIButton {
   
   // MARK: - INITIALIZERS
   override init(frame: CGRect) {
     super.init(frame: frame)
+    configure() 
+    self.backgroundColor = Constant.Color.buttonBackground
   }
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
   
+  // MARK: - CUSTOM FUNCTIONS
+  private func configure() {
+    layer.cornerRadius = 25
+    titleLabel?.textColor = .white
+    titleLabel?.font = Constant.Font.poppinsRegular20
+    translatesAutoresizingMaskIntoConstraints = false
+  }
 }
--- TourMap/SetupPage/View/SetupViewController.swift
+++ TourMap/SetupPage/View/SetupViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class SetupViewController: UIViewController {
   
+  let setupPage = SetupViews()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .white
+    configureSetupPage()
+  }
+  func configureSetupPage() {
+    setupPage.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(setupPage)
+
+    NSLayoutConstraint.activate([
+      setupPage.topAnchor.constraint(equalTo: view.topAnchor),
+      setupPage.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+      setupPage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      setupPage.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+    ])
   }
   
 }
