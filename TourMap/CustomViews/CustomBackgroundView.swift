152cdf2342d1c5339abc856a2c335f6940c2a877
Temitope <temitope.babatunde@decagon.dev>
2022-01-29 00:16:31 +0100

    created custom label

---
+++ TourMap/CustomViews/CustomBackgroundView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomBackgroundView.swift
+//  TourMap
+//
+//  Created by Decagon on 28/01/2022.
+//
+
+import UIKit
+
+class CustomBackgroundView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ TourMap/SignUp/Model/SignUpViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpViewModel.swift
+//  TourMap
+//
+//  Created by Decagon on 28/01/2022.
+//
+
+import Foundation
