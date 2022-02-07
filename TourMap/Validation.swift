91ad368511a8ef7e103fb611d8c7534c15ee20d1
Temitope <temitope.babatunde@decagon.dev>
2022-02-07 22:39:47 +0100

    refactor login page

--- TourMap/Validation.swift
+++ TourMap/Validation.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import UIKit
+
+class Validation {
+  //MARK:- Email Address Validation
+  func isValidEmail(email: String) -> Bool {
+    let regex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+    let emailPredicate = NSPredicate(format: "SELF MATCHES %@", regex)
+    let result = emailPredicate.evaluate(with: email)
+    return result
+  }
+  //MARK:- UserName Validation
+  func isValidName(name: String) -> Bool {
+    let regex = "[A-Za-z]{2,}"
+    let testPredicate = NSPredicate(format: "SELF MATCHES %@", regex)
+    let result = testPredicate.evaluate(with: name)
+    return result
+  }
+  
+  //    length 6 to 16.
+  //    One Alphabet in Password.
+  //    One Special Character in Password.
+  func isValidPassword(password: String) -> Bool {
+    let passwordRegEx = "^(?=.*?[A-Z])(?=.*[a-z])(?=.*?[0-9])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,16}"
+    let passwordPredicate = NSPredicate(format:"SELF MATCHES %@", passwordRegEx)
+    let result = passwordPredicate.evaluate(with: password)
+    return result
+  }
+
+  //MARK:- PhoneNumber Validation "^[0-9]{11}$"
+  func isValidPhoneNumber(_ PhoneNumber : String) -> Bool{
+    let phoneRegex = "^\\d{3}-\\d{3}-\\d{4}$"
+    let phonePredicate = NSPredicate(format: "SELF MATCHES %@", phoneRegex)
+    let result =  phonePredicate.evaluate(with: PhoneNumber)
+    return result
+  }  
+}
